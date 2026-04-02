8cf04a3263f309994bee1d4e1ba8b65201f28d5d
Paul Page <pdpagejr@alaska.edu>
2026-04-01 16:26:54 -0800

    Add OLED display support via PicoSpeakerOLED.h

--- cs241-bluetooth-speaker.ino
+++ cs241-bluetooth-speaker.ino
@@ -1,23 +1,32 @@
 #include <BluetoothAudio.h>
+#include <hardware/pwm.h>
+#include "PicoSpeakerOLED.h"
+#define PWM_PIN 14  
 
-#include "tones.h"
-
-PWMAudio pwm(14, false);
+// OLED I2C: GP4 = SDA, GP5 = SCL  (defined inside PicoSpeakerOLED.h)
+PWMAudio pwm(PWM_PIN, false);
 BluetoothAudioConsumerPWM consumer(pwm);
 A2DPSink a2dp;
-
 
 void setup() {
     Serial.begin(115200);
 
     a2dp.setName("PicoSpeaker");
-
-    setup_tones(a2dp);
-
     a2dp.setConsumer(&consumer);
     consumer.setVolume(255);
 
+    // Hand a2dp to the OLED module 
+    setup_oled(a2dp);
+
     a2dp.begin();
+
+    // Invert complement PWM 
+    uint slice = pwm_gpio_to_slice_num(PWM_PIN);
+    pwm_set_output_polarity(slice, false, true);
+
+    Serial.println(F("PicoSpeaker ready"));
 }
 
-void loop() {}
+void loop() {
+    loop_oled();   // metadata polling and OLED refresh
+}
